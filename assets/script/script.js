45345fa9e05012333520e66982d8eb1101cd6858
Paul Samson <sturmann333@gmail.com>
2021-02-26 12:56:56 +0300

    modify

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,17 @@
 'use strict'
 
 class Cart {
-	constructor(parentSelector,image,alt,model,text,prise){
-		this.parent=document.querySelector(parentSelector);
-		this.image=image;
-		this.alt=alt;
-		this.model=model;
-		this.text=text;
-		this.prise=prise;
-		}
-		render (){
-			const elememt = document.createElement("div");
-			elememt.innerHTML =`<article class="slider_items">
+    constructor(parentSelector, image, alt, model, text, prise) {
+        this.parent = document.querySelector(parentSelector);
+        this.image = image;
+        this.alt = alt;
+        this.model = model;
+        this.text = text;
+        this.prise = prise;
+    }
+    render() {
+        const elememt = document.createElement("div");
+        elememt.innerHTML = `<article class="slider_items">
 				<div class="image_velo"><img src=${this.image} alt=${this.alt}></div>
 				<div class="about_items">
 					<div class="about_velo">
@@ -22,44 +22,43 @@ class Cart {
 					<a href="#"><div class="btn_sold"><p class="add_to_bad">Add to Shopping bag</p></div></a>
 				 </div>
 			</article>`;
-			this.parent.append(elememt);
+        this.parent.append(elememt);
 
 
-		}
+    }
 }
 
-new Cart (
-	'.slider_velo',
-	"assets/images/bikes/bikes_01.jpg",
-	'gopescript',
-	'Sport Bicycle 988',
-	'by Enrique Salmo',
-	150
-	).render();
+// new Cart (
+// 	'.slider_velo',
+// 	"assets/images/bikes/bikes_01.jpg",
+// 	'gopescript',
+// 	'Sport Bicycle 988',
+// 	'by Enrique Salmo',
+// 	150
+// 	).render();
 
 
 const banner = document.querySelector(".banner"),
-tabs = document.querySelectorAll(".image_slider a");
+    tabs = document.querySelectorAll(".image_slider a");
 
-console.dir(tabs);
+// console.dir(tabs);
 
-function showAss (key){
-		//banner.classList = ["banner"];
-		
-		banner.className = "banner";
-		banner.classList.add(`ass`+ key);
+function showAss(key) {
+    //banner.classList = ["banner"];
+
+    banner.className = "banner";
+    banner.classList.add(`ass` + key);
 }
 
 
-tabs.forEach((item,key)=>{
-	item.addEventListener('click', (event)=>{
-		const target = event.target;
-		event.preventDefault();
+tabs.forEach((item, key) => {
+    item.addEventListener('click', (event) => {
+        const target = event.target;
+        event.preventDefault();
 
-		if (target){
-		showAss (key)
-		
-	}
-	})
-} );
+        if (target) {
+            showAss(key)
 
+        }
+    })
+});
